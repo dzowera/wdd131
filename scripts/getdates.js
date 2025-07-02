f85b58f087dabe068fcda4c347e1a9a11bf32ce7
dzowera <idzowera@byupathway.edu>
2025-07-02 08:50:40 +0200

    almost done, good to go

---
+++ scripts/getdates.js
@@ -0,0 +1,5 @@
+const current_year = new Date().getFullYear();
+document.getElementById("currentyear").textContent = current_year;
+
+const LastmodifiedDate = document.lastModified;
+document.getElementById("lastModified").textContent = LastmodifiedDate;
